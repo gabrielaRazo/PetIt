2607fe342ec78a4715db298eb618463d008bb579
Yunuen Moncada <moncadayunuen@gmail.com>
2020-01-19 18:24:08 -0600

    Index publico actualizado

--- js/main.js
+++ js/main.js
@@ -11,6 +11,37 @@ const menuTop = () => {
     navbarPetit.style.background = 'transparent';
     
   }
-  
-
 }
+
+$('.owl-one').owlCarousel({
+  margin: 10,
+  nav: true,
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 3
+      },
+      1000: {
+          items: 3
+      }
+  }
+});
+
+$('.owl-two').owlCarousel({
+  margin: 10,
+  loop: true,
+  nav: true,
+  responsive: {
+      0: {
+          items: 1
+      },
+      768: {
+          items: 3
+      },
+      1000: {
+          items: 5
+      }
+  }
+});
