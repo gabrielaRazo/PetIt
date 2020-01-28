066bee49b8f6414749655d35f1e6cfb54c7837cc
MarlenLuna <masluss2903@gmail.com>
2020-01-28 17:57:26 +0700

    deleted newpet, added mypatient.php,client.js and client.php

--- js/mypets.js
+++ js/mypets.js
@@ -4,15 +4,21 @@ var data = [
   {
     "petPic": "assets/PNG/perrito.png",
     "petName":"siri",
-    "petId":"3"
+    "petId":"3",
+    "clientId":"3",
+    "vetId":"5"
   },{
     "petPic": "assets/PNG/perrito.png",
     "petName":"pablo",
-    "petId":"2"
+    "petId":"2",
+    "clientId":"3",
+    "vetId":"5"
   },{
     "petPic": "assets/PNG/new-03.png",
     "petName":"siri",
-    "petId":"1"
+    "petId":"1",
+    "clientId":"3",
+    "vetId":"5"
     }
 ]
 
@@ -26,6 +32,12 @@ function petButtonVaccines(value){
 function petCalendarButton(value){
   var calendarBtn = '<button type="button" onclick="window.location.href =\'/index.html?petId=' + value + '\';" class="btn btn-outline-primary calendarBtn">VER   <i class="fas fa-eye eyeBtn"></i></button>'
   return calendarBtn;
+}
+
+function petTwoBtns(value){
+  var cartillaBtn = '<button type="button" onclick="window.location.href =\'/mimascota.php?petId=' + value + '\';" class="btn btn-outline-success cartillaBtn"><i class="fas fa-eye"></i></button>'
+  var deleteBtn = '<button type="button" onclick="window.location.href =\'/index.html?petId=' + value + '\';" class="btn btn-outline-danger deleteBtn"><i class="fas fa-trash-alt"></i></button>'
+  return cartillaBtn + deleteBtn;
 }
 
 function petThreeBtns(value){
